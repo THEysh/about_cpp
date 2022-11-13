cf5cc4c38c360ac1b935559e171a7f995ed0e00f
THEysh <1084701312@QQ.COM>
2022-11-14 00:09:23 +0800

    更新

---
+++ project6/funciton.cpp
@@ -0,0 +1,7 @@
+//
+// Created by top on 2022-11-13.
+//
+
+#include <iostream>
+#include "funciton.h"
+using namespace std;
---
+++ project6/funciton.h
@@ -0,0 +1,11 @@
+//
+// Created by top on 2022-11-13.
+//
+
+#ifndef PROJECT6_FUNCITON_H
+#define PROJECT6_FUNCITON_H
+
+
+
+
+#endif //PROJECT6_FUNCITON_H
