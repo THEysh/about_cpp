d11aec71f2325a306389b3a53289f49c84d0344f
THEysh <1084701312@QQ.COM>
2022-12-04 22:59:25 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "kl" << std::endl;
+    std::cout << "lad" << std::endl;
     std::string abc = "abdgf";
     std::string s2(abc);
     cout<<&abc<< endl;
