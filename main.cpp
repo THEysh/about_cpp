bb5b7cc2c316e03e3afeae228c9ca85cc5c69163
THEysh <1084701312@QQ.COM>
2022-12-22 18:12:23 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
     std::cout << "fsdfj" << std::endl;
-    std::string abc = "aksgaf";
+    std::string abc = "aksaf";
     std::string s2(abc);
     cout<<&abc<< endl;
     cout<<&s2<<endl;
