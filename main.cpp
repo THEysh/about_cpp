f7693ac489a48e8a866a35bc61444370a365e789
THEysh <1084701312@QQ.COM>
2022-12-22 18:16:17 +0800

    更新

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "fsdfj" << std::endl;
+    std::cout << "fsfj" << std::endl;
     std::string abc = "ak=sf";
     std::string s2(abc);
     cout<<&abc<< endl;
